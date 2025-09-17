18372e92fe0703aec058e176837e4f83069ce9e3
ZuomingWang <130771105+ZuomingWang@users.noreply.github.com>
2025-09-17 18:13:15 +0000

    Deploying to gh-pages from @ ZuomingWang/zuomingwang.github.io@83b29e17944c80c0a0a7f009f5c2987a967ef198 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection.",
+          description: "a growing collection.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
